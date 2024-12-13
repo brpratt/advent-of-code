48d9de541047ebee3886b517d00ac627b8d724d4
Ben Pratt <benjaminrpratt@gmail.com>
2024-12-13 16:12:43 -0600

    finish 2024 day 13

--- 2024/13/main.go
+++ 2024/13/main.go
@@ -36,92 +36,46 @@ func parseInput(r io.Reader) []game {
 	return games
 }
 
-type combination struct {
-	buttonA int
-	buttonB int
-}
-
-func solve(g game) []combination {
-	var (
-		cache map[grid.Point]map[combination]struct{}
-		iter  func(p grid.Point) map[combination]struct{}
-	)
-
-	cache = make(map[grid.Point]map[combination]struct{})
-
-	iter = func(p grid.Point) map[combination]struct{} {
-		if cached, ok := cache[p]; ok {
-			return cached
-		}
-
-		if p == g.prize {
-			set := make(map[combination]struct{})
-			set[combination{buttonA: 0, buttonB: 0}] = struct{}{}
-			return set
-		}
-
-		if p.X > g.prize.X || p.Y > g.prize.Y {
-			return make(map[combination]struct{})
-		}
-
-		posA := grid.Point{
-			X: p.X + g.buttonA.X,
-			Y: p.Y + g.buttonA.Y,
-		}
-
-		posB := grid.Point{
-			X: p.X + g.buttonB.X,
-			Y: p.Y + g.buttonB.Y,
-		}
-
-		combosA := iter(posA)
-		combosB := iter(posB)
-
-		combinations := make(map[combination]struct{})
-		for combo := range combosA {
-			combo.buttonA++
-			combinations[combo] = struct{}{}
-		}
-		for combo := range combosB {
-			combo.buttonB++
-			combinations[combo] = struct{}{}
-		}
-
-		cache[p] = combinations
-		return combinations
+func solve(a1, b1, c1, a2, b2, c2 int) (int, int) {
+	determinant := a1*b2 - a2*b1
+	if determinant == 0 {
+		return 0, 0
 	}
 
-	set := iter(grid.Point{X: 0, Y: 0})
-	combinations := make([]combination, 0)
-	for combo := range set {
-		combinations = append(combinations, combo)
-	}
+	x := (c1*b2 - c2*b1) / determinant
+	y := (a1*c2 - a2*c1) / determinant
 
-	return combinations
+	return x, y
 }
 
-func cost(c combination) int {
-	return (c.buttonA * 3) + c.buttonB
+type presses struct {
+	a int
+	b int
+}
+
+func play(g game) presses {
+	a, b := solve(g.buttonA.X, g.buttonB.X, g.prize.X, g.buttonA.Y, g.buttonB.Y, g.prize.Y)
+
+	if (g.buttonA.X*a)+(g.buttonB.X*b) != g.prize.X {
+		return presses{a: 0, b: 0}
+	}
+	if (g.buttonA.Y*a)+(g.buttonB.Y*b) != g.prize.Y {
+		return presses{a: 0, b: 0}
+	}
+
+	return presses{a: a, b: b}
+}
+
+func cost(c presses) int {
+	return (c.a * 3) + c.b
 }
 
 func part01(games []game) int {
 	var sum int
 
 	for _, game := range games {
-		combinations := solve(game)
-		if len(combinations) == 0 {
-			continue
-		}
-
-		cheapest := cost(combinations[0])
-		for i := 1; i < len(combinations); i++ {
-			c := cost(combinations[i])
-			if c < cheapest {
-				cheapest = c
-			}
-		}
-
-		sum += cheapest
+		combination := play(game)
+		sum += cost(combination)
 	}
 
 	return sum
@@ -133,21 +87,8 @@ func part02(games []game) int {
 	for _, game := range games {
 		game.prize.X += 10000000000000
 		game.prize.Y += 10000000000000
-
-		combinations := solve(game)
-		if len(combinations) == 0 {
-			continue
-		}
-
-		cheapest := cost(combinations[0])
-		for i := 1; i < len(combinations); i++ {
-			c := cost(combinations[i])
-			if c < cheapest {
-				cheapest = c
-			}
-		}
-
-		sum += cheapest
+		combination := play(game)
+		sum += cost(combination)
 	}
 
 	return sum
@@ -157,8 +98,8 @@ func main() {
 	input := file.Must(os.Open("input.txt"))
 	defer input.Close()
 
-	stones := parseInput(input)
+	games := parseInput(input)
 
-	fmt.Println(part01(stones))
-	fmt.Println(part02(stones))
+	fmt.Println(part01(games))
+	fmt.Println(part02(games))
 }
--- 2024/13/main_test.go
+++ 2024/13/main_test.go
@@ -46,3 +46,42 @@ Prize: X=18641, Y=10279`
 		}
 	})
 }
+
+func TestPart02(t *testing.T) {
+	t.Run("example", func(t *testing.T) {
+		input := `Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279`
+
+		expected := 875318608908
+		got := part02(parseInput(strings.NewReader(input)))
+
+		if got != expected {
+			t.Fatalf("[expected %d] [got %d]", expected, got)
+		}
+	})
+
+	t.Run("actual", func(t *testing.T) {
+		input := file.Must(os.Open("input.txt"))
+		defer input.Close()
+
+		expected := 96787395375634
+		got := part02(parseInput(input))
+
+		if got != expected {
+			t.Fatalf("[expected %d] [got %d]", expected, got)
+		}
+	})
+}
