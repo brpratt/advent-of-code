819497144b3251575cac05d9f2dc950cf6707f44
Ben Pratt <benjaminrpratt@gmail.com>
2025-12-01 07:18:07 -0600

    add 2025 day 01 part 02

--- 2025/01/main.go
+++ 2025/01/main.go
@@ -32,17 +32,72 @@ func parseRotations(r io.Reader) []int {
 	return rotations
 }
 
-func part01(rotations []int) int {
-	zeroCount := 0
-	pos := 50
-	for _, rotation := range rotations {
-		pos += rotation
-		pos %= 100
+func rotate(pos, rotation int) (int, int) {
+	var (
+		startedAtZero     = pos == 0
+		passesThroughZero = 0
+	)
+
+	pos += rotation
+
+	for pos >= 100 {
+		pos -= 100
+		passesThroughZero++
+	}
+
+	for pos < 0 {
+		pos += 100
+		passesThroughZero++
+	}
+
+	if rotation < 0 {
+		// If we started at zero, any rotation left will cause us to wrap around and "see"
+		// a zero in the preceding 'for' loop. Since we only want to count new zeros, we
+		// need to remove this phantom zero.
+		if startedAtZero {
+			passesThroughZero--
+		}
+
+		// If we're now at zero and we rotated left, the preceding 'for' loop wouldn't see
+		// this zero since position never became negative.
 		if pos == 0 {
-			zeroCount++
+			passesThroughZero++
+		}
+
+	}
+
+	return pos, passesThroughZero
+}
+
+func part01(rotations []int) int {
+	var (
+		count = 0
+		pos   = 50
+	)
+
+	for _, rotation := range rotations {
+		pos, _ = rotate(pos, rotation)
+		if pos == 0 {
+			count++
 		}
 	}
-	return zeroCount
+
+	return count
+}
+
+func part02(rotations []int) int {
+	var (
+		count             = 0
+		pos               = 50
+		passesThroughZero = 0
+	)
+
+	for _, rotation := range rotations {
+		pos, passesThroughZero = rotate(pos, rotation)
+		count += passesThroughZero
+	}
+
+	return count
 }
 
 func main() {
@@ -51,4 +106,5 @@ func main() {
 
 	rotations := parseRotations(input)
 	fmt.Println(part01(rotations))
+	fmt.Println(part02(rotations))
 }
--- 2025/01/main_test.go
+++ 2025/01/main_test.go
@@ -45,3 +45,41 @@ L82`
 		}
 	})
 }
+
+func TestPart02(t *testing.T) {
+	t.Run("example", func(t *testing.T) {
+		input := `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`
+
+		rotations := parseRotations(strings.NewReader(input))
+
+		expected := 6
+		got := part02(rotations)
+
+		if got != expected {
+			t.Fatalf("expected %d, got %d", expected, got)
+		}
+	})
+
+	t.Run("actual", func(t *testing.T) {
+		input := file.Must(os.Open("input.txt"))
+		defer input.Close()
+
+		rotations := parseRotations(input)
+
+		expected := 6768
+		got := part02(rotations)
+
+		if got != expected {
+			t.Fatalf("expected %d, got %d", expected, got)
+		}
+	})
+}
