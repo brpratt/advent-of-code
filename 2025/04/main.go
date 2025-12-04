ac86e7e27bc68bef86605acd4896f610caa3bdb9
Ben Pratt <benjaminrpratt@gmail.com>
2025-12-04 04:22:24 -0600

    add 2025 day 04 part 01

---
+++ 2025/04/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/brpratt/advent-of-code/file"
+	"github.com/brpratt/advent-of-code/grid"
+)
+
+func parseGrid(r io.Reader) grid.Grid[byte] {
+	var g grid.Grid[byte]
+
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		row := []byte(scanner.Text())
+		g = append(g, row)
+	}
+
+	return g
+}
+
+func adjacentPoints(p grid.Point) []grid.Point {
+	return []grid.Point{
+		grid.Move(p, grid.Up),
+		grid.Move(p, grid.UpRight),
+		grid.Move(p, grid.Right),
+		grid.Move(p, grid.DownRight),
+		grid.Move(p, grid.Down),
+		grid.Move(p, grid.DownLeft),
+		grid.Move(p, grid.Left),
+		grid.Move(p, grid.UpLeft),
+	}
+}
+
+func adjancentRolls(p grid.Point, g grid.Grid[byte]) int {
+	var count int
+
+	for _, ap := range adjacentPoints(p) {
+		value, _ := g.Value(ap)
+
+		if value == '@' {
+			count++
+		}
+	}
+
+	return count
+}
+
+func part01(g grid.Grid[byte]) int {
+	var count int
+
+	for y := range g {
+		for x := range len(g[y]) {
+			p := grid.Point{X: x, Y: y}
+
+			value, _ := g.Value(p)
+			if value == '@' && adjancentRolls(p, g) < 4 {
+				count++
+			}
+		}
+	}
+
+	return count
+}
+
+func main() {
+	input := file.Must(os.Open("input.txt"))
+	defer input.Close()
+
+	g := parseGrid(input)
+
+	fmt.Println(part01(g))
+}
---
+++ 2025/04/main_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/brpratt/advent-of-code/file"
+)
+
+func TestPart01(t *testing.T) {
+	t.Run("example", func(t *testing.T) {
+		input := `..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.`
+
+		g := parseGrid(strings.NewReader(input))
+
+		expected := 13
+		got := part01(g)
+
+		if expected != got {
+			t.Fatalf("expected %d, got %d", expected, got)
+		}
+	})
+
+	t.Run("actual", func(t *testing.T) {
+		input := file.Must(os.Open("input.txt"))
+		defer input.Close()
+
+		g := parseGrid(input)
+
+		expected := 1486
+		got := part01(g)
+
+		if expected != got {
+			t.Fatalf("expected %d, got %d", expected, got)
+		}
+	})
+}
